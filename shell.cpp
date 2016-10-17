5e05a507aa267ff4e9fc9932394e0269377bf332
David Browning <dmbrowning21@gmail.com>
2016-10-17 21:00:36 +0000

    "formatting"

--- shell.cpp
+++ shell.cpp
@@ -75,16 +75,16 @@ void shell::changeDirectories(std::vector<std::string> cmd){
 
 
 void shell::handlePipe(std::vector<std::string> cmd1, std::vector<std::string> cmd2){
- std::cout << "I handled the pipe! -not" << std::endl;
- for(std::string e : cmd1){
-  std::cout << "cmd 1: " <<  e << std::endl;
- }
- for(std::string e : cmd2){
- std::cout << "cmd 2: " << e << std::endl;
- }
- std::cout << "Getting the argv arrays set up" << std::endl;
+ //std::cout << "I handled the pipe! -not" << std::endl;
+ //for(std::string e : cmd1){
+ // std::cout << "cmd 1: " <<  e << std::endl;
+ //}
+ //for(std::string e : cmd2){
+ //std::cout << "cmd 2: " << e << std::endl;
+ //}
+ //std::cout << "Getting the argv arrays set up" << std::endl;
 
- std::cout << "supposedly, I have both argv1 and argv2 ready to go" << std::endl;
+ //std::cout << "supposedly, I have both argv1 and argv2 ready to go" << std::endl;
 
 
   char** argv = new char*[(cmd1.size()+1)];
@@ -110,74 +110,40 @@ void shell::handlePipe(std::vector<std::string> cmd1, std::vector<std::string> c
   }
  i = (cmd2.size());
  argv2[i] = NULL;
- std::cout << "argv[0]: " << argv[0] << std::endl;
- std::cout << "argv2[0]: " << argv2[0] << std::endl;
+// std::cout << "argv[0]: " << argv[0] << std::endl;
+// std::cout << "argv2[0]: " << argv2[0] << std::endl;
  std::cout.flush();
- std::cout << "Awesome, now to execute stuff" << std::endl;
+// std::cout << "Awesome, now to execute stuff" << std::endl;
  
  const int PIPE_COUNT = 2;
-
  const int PIPE_READ_END = 0;
-
  const int PIPE_WRITE_END = 1;
-
-
-
  const int STDIN = 0;
-
  const int STDOUT = 1;
+ int pids[PIPE_COUNT];
+ pipe(pids);
 
-	int pids[PIPE_COUNT];
-	pipe(pids);
+ int savedStdout = dup(STDOUT);
+ int savedStdin = dup(STDIN);
 
-	int savedStdout = dup(STDOUT);
-	int savedStdin = dup(STDIN);
-
-	//
-	// First child will output the source code to this program to the pipe
-	pid_t pid = fork();
-	if (pid == 0)
-	{
-		dup2(pids[PIPE_WRITE_END], STDOUT);
-
-		execvp(argv[0], argv);
-	}
-
-	//
-	// Second child will 'more' whatever input comes down over the pipe
-	pid_t pid2 = fork();
-	if (pid2 == 0)
-	{
-		dup2(pids[PIPE_READ_END], STDIN);
-		//
-		// This is key, in order to terminate the input from the pipe
-		// have to close off the write end, otherwise the 'more' command
-		// will continue to wait for additional data.
-		close(pids[PIPE_WRITE_END]);
-
-		execvp(argv2[0], argv2);
-	}
-
-	//
-	// Wait for the first child to finish
-	int status;
-	waitpid(pid, &status, 0);
-
-	//
-	// Fully close down the pipe, and yes, for whatever reason, it requires
-	// the parent process to close both ends, even though the second child
-	// already closed the write end...not sure I fully understand this.
-	close(pids[PIPE_WRITE_END]);
-	close(pids[PIPE_READ_END]);
-
-	waitpid(pid2, &status, 0);
-
-	//
-	// Restore standard out and in, so our program will be back to normal when complete
-	dup2(savedStdout, STDOUT);
-	dup2(savedStdin, STDIN);
-
-
+ pid_t pid = fork();
+ if (pid == 0){
+  dup2(pids[PIPE_WRITE_END], STDOUT);
+  execvp(argv[0], argv);
+ }
+ pid_t pid2 = fork();
+ if (pid2 == 0){
+  dup2(pids[PIPE_READ_END], STDIN);
+  close(pids[PIPE_WRITE_END]);
+  execvp(argv2[0], argv2);
+ }
+ int status;
+ waitpid(pid, &status, 0);
+ close(pids[PIPE_WRITE_END]);
+ close(pids[PIPE_READ_END]);
+ waitpid(pid2, &status, 0);
+ dup2(savedStdout, STDOUT);
+ dup2(savedStdin, STDIN);
  return;
 }
 
